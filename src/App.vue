32ef86450253a82e067cb0a790ebaa1e77da412e
louislasserre <louislasserre@mmibordeaux.com>
2022-06-18 17:39:57 +0200

    lighthouse upgrades

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,21 @@ export default {
 </script>
 
 <style lang="scss">
+  @font-face {
+    font-family: "Lausanne-300";
+    src: url('@/fonts/TWKLausanne-300.7842412.woff2') format("woff2");
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Lausanne-400";
+    src: url('@/fonts/TWKLausanne-400.3ab9e07.woff2') format("woff2");
+    font-display: swap;
+  }
+  @font-face {
+    font-family: "Lausanne-500";
+    src: url('@/fonts/TWKLausanne-500.e59d791.woff2') format("woff2");
+    font-display: swap;
+  }
   @import '@/assets/style/variables';
   @import "@/assets/style/components/mainComponents";
   @import "@/assets/style/pages/home";
@@ -45,21 +60,7 @@ export default {
   @import "@/assets/style/base.css";
   @import "@/assets/style/components/footer";
 
-  @font-face {
-    font-family: "Lausanne-300";
-    src: url('/src/fonts/TWKLausanne-300.7842412.woff2') format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: "Lausanne-400";
-    src: url('/src/fonts/TWKLausanne-400.3ab9e07.woff2') format("woff2");
-    font-display: swap;
-  }
-  @font-face {
-    font-family: "Lausanne-500";
-    src: url('/src/fonts/TWKLausanne-500.e59d791.woff2') format("woff2");
-    font-display: swap;
-  }
+
 
 
 #app {
